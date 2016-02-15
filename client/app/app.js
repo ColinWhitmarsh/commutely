ac6794e7559a74fb63c24a95c10aed9eab6494f0
Hack Reactor Students <accounts@hackreactor.com>
2016-02-15 15:06:57 -0800

    navigate between login and signup angular views

--- client/app/app.js
+++ client/app/app.js
@@ -1,12 +1,19 @@
-angular.module('commutely', [])
+angular.module('commutely', [
+  'commutely.services',
+  'commutely.auth',
+  'ngRoute'
+])
 .config(function ($routeProvider, $httpProvider) {
   $routeProvider
     .when('/login', {
       templateUrl: 'app/auth/login.html',
       controller: 'AuthController'
     })
-    .when('signup', {
+    .when('/signup', {
       templateUrl: 'app/auth/signup.html',
       controller: 'AuthController'
+    })
+    .otherwise({
+      redirectTo: '/login'
     });
 });
---
+++ client/app/auth/auth.js
@@ -0,0 +1,26 @@
+angular.module('commutely.auth', [])
+.controller('AuthController', function ($scope, $window, $location, Auth) {
+  $scope.user = {};
+
+  $scope.signin = function () {
+    Auth.signin($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.commutely', token);
+        $location.path('/commute');
+      })
+      .catch( function (error) {
+        console.error(error);
+      });
+  };
+
+  $scope.signup = function () {
+    Auth.signup($scope.user)
+      .then(function (token) {
+        $window.localStorage.setItem('com.shortly', token);
+        $location.path('/links');
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+});
